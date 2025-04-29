45852bff869bbaad8026931508ab9f624c41553d
victoria tran <108353792+victoriatr6n@users.noreply.github.com>
2025-04-29 00:52:08 -0700

    fix buggy code

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,10 +7,7 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
-
-        result = num1 + num2;
-
+        let result = num1 + num2;
         return result;
     }
     else {
@@ -25,11 +22,14 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if(prices.length = 0){
+        return false
+    }
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
